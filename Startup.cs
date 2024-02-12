fb06ab1f164e8b61fe02fae601a1185cffcfb371
22010446NqobaniKhanyile <127221609+22010446NqobaniKhanyile@users.noreply.github.com>
2024-02-12 13:41:59 +0200

    First

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataVic.Startup))]
+namespace DataVic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
